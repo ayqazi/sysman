1078a45530f13c7712632d3c530ed1d5933186a1
Asfand Yar Qazi <ayqazi@gmail.com>
2013-11-16 01:36:16 +0000

    Do not use aptitude in setup_ansible.sh

--- bin/setup_ansible.sh
+++ bin/setup_ansible.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-aptitude -y install python-software-properties
+apt-get -y install python-software-properties
 add-apt-repository -y ppa:rquillo/ansible
 apt-get update
 apt-get -y install ansible
