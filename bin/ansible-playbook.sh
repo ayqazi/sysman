7ed577ccf5de614a42d9d07bc910f4f33d8f193e
Asfand Yar Qazi <ayqazi@gmail.com>
2014-02-03 09:19:19 +0000

    Make running bin/ansible-playbook.sh script less tortuous

--- bin/ansible-playbook.sh
+++ bin/ansible-playbook.sh
@@ -3,5 +3,6 @@
 set -e
 
 source /opt/sysman/bin/load_env.sh
-
-exec ansible-playbook "$@"
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
+PLAYBOOK="${DIR}/etc/ansible/playbooks/sysman.yml"
+exec ansible-playbook "${PLAYBOOK}" -e "dir='${DIR}'" "$@"
