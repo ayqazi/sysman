c09be6db788817d8fa98763bbcab1b7935dce934
Asfand Yar Qazi <ayqazi@gmail.com>
2015-04-30 12:37:43 +0100

    Load hosts file automatically in bin/run.sh

--- bin/run.sh
+++ bin/run.sh
@@ -5,4 +5,5 @@ set -e
 source /opt/sysman/bin/load_env.sh
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )"/.. && pwd )"
 PLAYBOOK="${DIR}/etc/ansible/playbooks/sysman.yml"
-exec ansible-playbook "${PLAYBOOK}" -e "dir='${DIR}'" "$@"
+HOSTS="${DIR}/etc/ansible/hosts"
+exec ansible-playbook "${PLAYBOOK}" --inventory-file="${HOSTS}" -e "dir='${DIR}'" "$@"
